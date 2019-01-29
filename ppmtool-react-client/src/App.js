f0d240fa6060e8e84a19e19b9ed330e679047bbd
Dsilva <doltondsilva23@gmail.com>
2019-01-29 16:03:37 +0530

    Adding redux dependencies

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard';
+import Header from './components/layout/Header';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProject from './components/project/AddProject';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard></Dashboard>
-      </div>
+      <Router>
+        <div className="App">
+          <Header></Header>
+          <Route exact path="/dashboard" component={Dashboard}></Route>
+          <Route exact path="/addProject" component={AddProject}></Route>
+        </div>
+      </Router>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem';
+import CreateProjectButton from './project/CreateProjectButton';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Dashboard</h1>
-      </div>
+        <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <CreateProjectButton></CreateProjectButton>
+                        
+                        <br />
+                        <hr />
+
+                        <ProjectItem></ProjectItem>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
   }
 }
